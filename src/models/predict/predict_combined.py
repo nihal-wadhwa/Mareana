81c5b46c4b439768e5b4b9bd3b4009ee449a0149
JampherS <shyam.paidipati@gmail.com>
2020-07-09 19:59:49 -0400

    Updated predict_combined file. Removed duplicate png files. NOTE: Path is hardcoded

--- src/models/predict/predict_combined.py
+++ src/models/predict/predict_combined.py
@@ -4,33 +4,30 @@ import argparse
 import pickle
 import sys
 import os
-
 # CHANGE TO SYSTEM PATH TO LOCALIZATION SCRIPT
-sys.path.append(os.path.abspath("~/mareana/mareana-repo/Localization"))
-from Localization import *
+sys.path.insert(1, '/home/jampher/Document-Symbol-Classification/Localization')
+from Localization import pre_processing, watershed_segmentation, filtering
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--image', type=str, default=None,
     help="Path of the image we want to classify")
 parser.add_argument("--model", type=str, default=None,
 	help="Type of model")
-
 args = vars(parser.parse_args())
-
 original, pre_processed = pre_processing(args['image'])
 original, segmented, label, statistics, numLabel = watershed_segmentation(original, pre_processed)
 original_img, labeled_img, bounding_box_array = filtering(original, segmented, label, statistics, numLabel)
 
 # load the model and label binarizer
 print("[INFO] loading network and label binarizer...")
-model = load_model("../../output/{}.model".format(args['model']))
-lb = pickle.loads(open("../../output/{}_lb.pickle".format(args['model']), "rb").read())
+model = load_model("../output/{}.model".format(args['model']))
+lb = pickle.loads(open("../output/{}_lb.pickle".format(args['model']), "rb").read())
 
 # make a prediction on the images
 for image in bounding_box_array:
 	output = image.copy()
 	image = cv2.resize(image, (32, 32))
-	image = symbol.astype("float") / 255.0
+	image = image.astype("float") / 255.0
 	# check to see if we should flatten the image and add a batch
 	# dimension
 	if args["model"] == 'nnet':
@@ -51,8 +48,8 @@ for image in bounding_box_array:
 	# draw the class label + probability on the output image
 	text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
 	print(text)
-	cv2.putText(output, text, (8, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
-		(0, 0, 255), 1, cv2.LINE_AA)
+	#cv2.putText(output, text, (8, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
+	#	(0, 0, 255), 1, cv2.LINE_AA)
 	# show the output image
-	cv2.imshow("Image", output)
-	cv2.waitKey(0)
+	#cv2.imshow("Image", output)
+	#cv2.waitKey(0)
