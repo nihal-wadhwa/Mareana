92c7084ba775aadc74c5b968f19ccc3ace8e8826
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-07-10 13:12:03 -0400

    Prediction file for document classification

--- src/models/predict/predict_combined.py
+++ src/models/predict/predict_combined.py
@@ -5,7 +5,7 @@ import pickle
 import sys
 import os
 # CHANGE TO SYSTEM PATH TO LOCALIZATION SCRIPT
-sys.path.insert(1, '/home/jampher/Document-Symbol-Classification/Localization')
+sys.path.insert(1, '/Users/dhruv/Desktop/Document-Symbol-Classification/Localization')
 from Localization import pre_processing, watershed_segmentation, filtering
 
 parser = argparse.ArgumentParser()
@@ -14,19 +14,20 @@ parser.add_argument('--image', type=str, default=None,
 parser.add_argument("--model", type=str, default=None,
 	help="Type of model")
 args = vars(parser.parse_args())
+
 original, pre_processed = pre_processing(args['image'])
 original, segmented, label, statistics, numLabel = watershed_segmentation(original, pre_processed)
 original_img, labeled_img, bounding_box_array = filtering(original, segmented, label, statistics, numLabel)
 
 # load the model and label binarizer
 print("[INFO] loading network and label binarizer...")
-model = load_model("../output/{}.model".format(args['model']))
-lb = pickle.loads(open("../output/{}_lb.pickle".format(args['model']), "rb").read())
+model = load_model("../../output/{}.model".format(args['model']))
+lb = pickle.loads(open("../../output/{}_lb.pickle".format(args['model']), "rb").read())
 
 # make a prediction on the images
 for image in bounding_box_array:
 	output = image.copy()
-	image = cv2.resize(image, (32, 32))
+	image = cv2.resize(image, (64, 64))
 	image = image.astype("float") / 255.0
 	# check to see if we should flatten the image and add a batch
 	# dimension
@@ -48,6 +49,7 @@ for image in bounding_box_array:
 	# draw the class label + probability on the output image
 	text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
 	print(text)
+	
 	#cv2.putText(output, text, (8, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
 	#	(0, 0, 255), 1, cv2.LINE_AA)
 	# show the output image
