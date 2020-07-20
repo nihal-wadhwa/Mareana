b061b69c1408db8d581a51a7bc96f35c873b7153
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-07-20 15:06:41 -0400

    [ADD] Script to save symbols with label

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import math
 import sys
 import argparse
 import numpy as np
@@ -9,7 +10,7 @@ from tensorflow.python.keras import models
 
 # CHANGE TO SYSTEM PATH TO LOCALIZATION SCRIPT
 sys.path.insert(1, '/Users/dhruv/Desktop/Document-Symbol-Classification/Localization')
-from Localization import pre_processing, segmentation, filtering, second_segmentation, drawBoundingBoxes, get_final_bounding_boxes
+from Localization import pre_processing, segmentation, filtering, second_segmentation, text_merging, get_final_bounding_boxes
 
 
 #extract the arguments 
@@ -70,33 +71,31 @@ if args.task == 'collect':
 
 
 
-def segment(doc):
-    pre_processed = pre_processing(doc)
+def segment(original):
+    resized_original, scale, pre_processed = pre_processing(original)
     bounding_boxes = segmentation(pre_processed)
     image_regions, text_regions = filtering(bounding_boxes)
     image_regions, text_regions = second_segmentation(image_regions, text_regions)
-    returned_bounding_boxes, bounding_box_locations = get_final_bounding_boxes(doc, image_regions)
-    image_labeled_img = drawBoundingBoxes(doc, image_regions, (128,0,128))
-    labeled_img = drawBoundingBoxes(image_labeled_img, text_regions, (0,255,0))
+    text_regions = text_merging(text_regions)
+    returned_bounding_boxes, bounding_box_locations, final_img = get_final_bounding_boxes(original, scale, image_regions)
 
     if args.task == 'segment':
         cv2.imshow('labels after second segmentation', labeled_img)
         cv2.waitKey(0)
     else:
-        return returned_bounding_boxes, bounding_box_locations, labeled_img
+        return returned_bounding_boxes, bounding_box_locations, final_img
 
 
 def predict(symbols, locations, original):
-    text = {}
-    model = models.load_model('src/output/vggnet.model')
-    lb = pickle.loads(open("src/output/vggnet_lb.pickle", "rb").read())
+    model = models.load_model('src/output/resnet.model')
+    lb = pickle.loads(open("src/output/resnet_lb.pickle", "rb").read())
     s = 0 # Symbol counter for retrieving label location
     # make a prediction on the images
     symbol_img = original.copy()
 
     for image in symbols:
         label_loc = locations[s]
-        image = cv2.resize(image, (64, 64))
+        image = cv2.resize(image, (32, 32))
         image = image.astype("float") / 255.0
         image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
         
@@ -120,7 +119,25 @@ def predict(symbols, locations, original):
     cv2.imshow("Image", symbol_img)
     cv2.waitKey(0)
 
-def collect(symbols)
+def collect(symbols):
+    model = models.load_model('src/output/resnet.model')
+    lb = pickle.loads(open("src/output/resnet_lb.pickle", "rb").read())
+
+    for symbol in symbols:
+        symbol = cv2.resize(symbol, (32, 32))
+        copy = symbol.copy()
+        symbol = symbol.astype("float") / 255.0
+        symbol = symbol.reshape((1, symbol.shape[0], symbol.shape[1], symbol.shape[2]))
+
+        preds = model.predict(symbol)
+
+        i = preds.argmax(axis=1)[0]
+        label = lb.classes_[i]
+
+        confidence = "{}_{:.0f}".format(label, (preds[0][i] * 100))
+        #img = cv2.imdecode(symbol, cv2.IMREAD_GRAYSCALE)
+
+        cv2.imwrite('output/{}.png'.format(confidence), copy)
 
 
 # call functions based on --task values
